fcb8a23b2c091100d25915f18fb888384471b876
Xcv <xcv.work@gmail.com>
2017-10-29 00:30:08 +0800

    分支合併後的第一次推送更新

--- Assets/Git整合測試.cs
+++ Assets/Git整合測試.cs
@@ -7,6 +7,8 @@ public class Git整合測試 : MonoBehaviour {
     //第一次Git整合上傳測試
     //第二次Git整合上傳測試_更新
     //第三次Git整合上傳測試_已知步驟:1.變更2.推送
+    //第四次Git整合上傳測試_在新的分支中推送
+    //takahiro
 	// Use this for initialization
 	void Start () {
 		
@@ -18,4 +20,5 @@ public class Git整合測試 : MonoBehaviour {
     }
     //第五次Git整合上傳測試
     //第五次Git整合上傳測試
+    //分支合併後的第一次推送更新
 }
